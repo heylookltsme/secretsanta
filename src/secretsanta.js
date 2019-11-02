c0eb4e5ffabf6aa9a21664ce496b33ba273becc8
Corinne Hoener <corinne@vimeo.com>
2019-11-02 10:19:18 -0400

    Add npm scripts, comments, and a test run flag

--- src/secretsanta.js
+++ src/secretsanta.js
@@ -3,6 +3,8 @@ import Twilio from 'twilio';
 import config from '../config';
 import peeps from '../people';
 
+const isTestRun = process.argv.includes('--test');
+
 const twilioClient = new Twilio(
   config.twilio.accountSid,
   config.twilio.authToken,
@@ -10,19 +12,35 @@ const twilioClient = new Twilio(
 
 const assignments = {};
 
-function assign(name) {
+/**
+ * Create a specific secret santa assignment for the given name.
+ *
+ * @param {string} name - The person to be assigned.
+ */
+const assign = (name) => {
+  // Determine exclusions: the person's exclude key + all those already assigned
   const exclusions = [name].concat(peeps[name].exclude, _.values(assignments));
+  // The assignment options are everyone not excluded.
   const assignmentOptions = _.difference(_.keys(peeps), exclusions);
+  // Pick an assignment at random.
   assignments[name] = _.shuffle(assignmentOptions).shift();
-}
+};
 
-function generateAssignments() {
+/**
+ * Generate all the secret santa assignments.
+ */
+const generateAssignments = () => {
   _.forEach(peeps, (value, name) => {
     assign(name);
   });
-}
+};
 
-function sendSms(name) {
+/**
+ * Send a text with the secret santa assignment.
+ *
+ * @param {string} name - The person to text.
+ */
+const sendSms = (name) => {
   const message = `Hi there, ${_.capitalize(
     name,
   )}! Your 2019 Secret Santa assignment is: ${_.capitalize(
@@ -37,8 +55,9 @@ function sendSms(name) {
       from: config.twilio.phoneNumber,
     })
     .then((m) => console.log(m.sid));
-}
+};
 
+// Generate the assignments
 while (
   _.isEmpty(assignments)
   || _.values(assignments).indexOf(undefined) >= 0
@@ -46,8 +65,16 @@ while (
   generateAssignments();
 }
 
-_.forEach(assignments, (value, key) => {
-  sendSms(key);
-});
+// If executed with --send-sms, then actually send the assignments out via sms.
+if (!isTestRun) {
+  _.forEach(assignments, (value, key) => {
+    sendSms(key);
+    console.log(`sent sms to ${key}`);
+  });
+}
 
+// Print out the assignments
+if (isTestRun) {
+  console.log('Test run assignments:');
+}
 console.log(assignments);
